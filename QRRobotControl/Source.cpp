645581689196ecb87da6fb802fd05582f1ecfa0f
slanab <borklana@gmail.com>
2016-07-07 14:08:47 -0700

    Added ability to perform multiple commands encoded in QR

--- QRRobotControl/Source.cpp
+++ QRRobotControl/Source.cpp
@@ -15,20 +15,27 @@ using namespace zbar;
 
 int port_num_telnet = 4444; 
 char *IP_ADD = "207.23.183.214"; // Robot IP
+string filename = "rtmp://207.23.183.214:1935/oculusPrime/stream1";
 
-void performCommand(string command) 
-{	
-	string response;
-	string commandEndl = command + '\n'; // Command has to end with a new line symbol
-	socketSend(commandEndl.data(), commandEndl.length());
-	response = socketResponse();
-	//cout << "Response received:\n**********\n" << response << "**********\n" << endl;
+void performCommand(string commands, int responseCycles) 
+{		
+	cout << "Sending commands: " << commands;
+	// Break down commands into one by one	
+	string command = "";
+	stringstream commandStream(commands);
+	while (getline(commandStream, command, '\n')) {
+		cout << "Single command: " << command;
+		command = command + '\n';	// Add new line to the command, else command won't be sent
+		socketSend(command.data(), command.length());
+		for (int i = 0; i < responseCycles; i++) {
+			string response = socketResponse();
+			cout << "Response received:\n**********\n" << response << "**********\n" << endl;
+		}
+	}
 }
 
 int main(int argc, char* argv[])
 {
-	string filename = "rtmp://207.23.183.214:1935/oculusPrime/stream1";
-
 	// Establishing socket connection
 	if (!connectToHost(port_num_telnet, IP_ADD))
 	{
@@ -36,9 +43,13 @@ int main(int argc, char* argv[])
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		return -1;
 	}	
+	string response;
+	response = socketResponse();
+	response = socketResponse();
 
 	// Obtaining robot's webcam stream	
-	socketSend("publish camera\n", 15);
+	performCommand("publish camera", 2); // 2 responses
+
 	VideoCapture cap(filename);
 	if (!cap.isOpened())  // if not success, exit program
 	{
@@ -50,10 +61,10 @@ int main(int argc, char* argv[])
 	// Processing the frames for the QR codes
 	ImageScanner scanner;
 	scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);		
-
+	int cycles = 0;
 	string command = "";
 	string lastCommand = "";
-	int cycles = 0;
+	
 	while (1)
 	{
 		Mat frame;
@@ -72,9 +83,9 @@ int main(int argc, char* argv[])
 		uchar *raw = (uchar *)grey.data;
 		Image image(width, height, "Y800", raw, width * height);
 
-		// scan the image for barcodes   
+		// Scan the image for QR codes   
 		int n = scanner.scan(image);
-		if (n == 0) { // Reset commands if no QR code is in the frame for 5 cycles (arbitrary value, might need to be adjusted)
+		if (n == 0) { // Reset commands if there is no QR code in the frame for 5 cycles (arbitrary value, might need to be adjusted)
 			if (cycles >= 5) { // Wait for 5 cycles, QR code is not detected each cycle
 				if (lastCommand != "" || command != "") {
 					cout << "No QR codes detected for 5 cycles, reset commands\n";
@@ -90,24 +101,20 @@ int main(int argc, char* argv[])
 			cycles = 0; // Reset cycles each time QR code is detected
 		}
 		
-		// extract results   
-		for (Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol) {
+		// Extract results of QR decoding, perform decoded command
+		for (SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol) {
 			lastCommand = command;
 			command = symbol->get_data();
-			cout << "Decoded " << symbol->get_type_name() << " symbol \"" << command << '"' << " " << endl;
-			cout << "Previous command was " << lastCommand << endl;
-			if (command != "") {			// Ignore repeating the same command over and over while QR code is in the camera frame
-				if (command != lastCommand) {
-					cout << "Perform command " << command << endl;
-					performCommand(command);
-				}
-				else {
-					cout << "Same command, ignore " << command << endl;				
-				}
+			cout << "Decoded command: " << command << endl;
+			cout << "Previous command was: " << lastCommand << endl;
+			if ((command != "") && (command != lastCommand)) {		// Ignore repeating the same command over and over while QR code is in the camera frame					
+				cout << "Perform command " << command << endl;
+				performCommand(command, 3);
 			}
 		}
 
-		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop  
+		// Wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop  
+		if (waitKey(30) == 27) 
 		{
 			cout << "esc key is pressed by user" << endl;
 			break;
