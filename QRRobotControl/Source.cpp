e12f1ba739ad3df185b41bb131b2c2ff46573e22
slanab <borklana@gmail.com>
2016-07-29 14:46:03 -0700

    Debugging angle correction problems.

--- QRRobotControl/Source.cpp
+++ QRRobotControl/Source.cpp
@@ -184,6 +184,15 @@ int main(int argc, char* argv[])
 		return -1;
 	}	
 
+	completeCommand("left 90");
+	waitForSocketResponse(1);
+	completeCommand("left 90");
+	waitForSocketResponse(1);
+	completeCommand("left 90");
+	waitForSocketResponse(1);
+	completeCommand("left 90");
+	waitForSocketResponse(1);
+
 	ImageScanner scanner;
 	scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);	
 
